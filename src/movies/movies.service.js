d850ea9a5a239c8b585404811f871df51206c35e
dylanalbios <dylxanj430@gmail.com>
2023-08-08 16:00:30 -0700

    added theatersBymovieId and reviewsWithCriticByMovieId functions

--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -2,7 +2,7 @@ const knex = require("../db/connection");
 
 const tableName = "movies";
 
-function list(isShowing) {
+const list = (isShowing) => {
     let query = knex(tableName).select("*");
 
     if (isShowing) {
@@ -14,11 +14,45 @@ function list(isShowing) {
     return query;
 };
 
-function read(movie_id) {
+const read = (movie_id) => {
     return knex(tableName).select("*").where({ movie_id }).first();
 };
 
+const theatersByMovieId = (movie_id) => {
+    return knex("movies_theaters")
+        .select("theaters.*")
+        .join("theaters", "movies_theaters.theater_id", "theaters.theater_id")
+        .where({ "movies_theaters.movie_id": movie_id });
+};
+
+const reviewsWithCriticByMovieId = async (movieId) => {
+    try {
+      const reviews = await knex("reviews as r")
+        .select(
+          "r.*",
+          "c.preferred_name as critic_preferred_name",
+          "c.surname as critic_surname",
+          "c.organization_name as critic_organization_name"
+        )
+        .join("critics as c", "r.critic_id", "c.critic_id")
+        .where("r.movie_id", movieId);
+  
+      return reviews.map((review) => ({
+        ...review,
+        critic: {
+          preferred_name: review.critic_preferred_name,
+          surname: review.critic_surname,
+          organization_name: review.critic_organization_name,
+        },
+      }));
+    } catch (error) {
+      throw error;
+    }
+};
+  
 module.exports = {
     list,
     read,
+    theatersByMovieId,
+    reviewsWithCriticByMovieId,
 };
