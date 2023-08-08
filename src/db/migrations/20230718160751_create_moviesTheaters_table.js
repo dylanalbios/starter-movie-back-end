8413ecd27cf4db379b15fa7bf781f89b1d7e31f9
dylanalbios <dylxanj430@gmail.com>
2023-08-08 11:47:33 -0700

    first commit

---
+++ src/db/migrations/20230718160751_create_moviesTheaters_table.js
@@ -0,0 +1,25 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable("movies_theaters", (table) => {
+    table.increments("movies-theaters_id").primary();
+    table.boolean("is_showing");
+    table.integer("theater_id").unsigned().notNullable();
+    table
+        .foreign("theater_id")
+        .references("theater_id")
+        .inTable("theaters")
+        .onDelete("CASCADE");
+
+    table.integer("movie_id").unsigned().notNullable();
+    table
+        .foreign("movie_id")
+        .references("movie_id")
+        .inTable("movies")
+        .onDelete("CASCADE");
+    table.timestamps(true, true);
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable("movies-theaters");
+};
