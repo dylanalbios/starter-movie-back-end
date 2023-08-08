2490998bf2f96bea953938d9d51125dd6ede95bc
dylanalbios <dylxanj430@gmail.com>
2023-08-08 14:19:32 -0700

    list and read functions added

---
+++ src/movies/movies.service.js
@@ -0,0 +1,24 @@
+const knex = require("../db/connection");
+
+const tableName = "movies";
+
+function list(isShowing) {
+    let query = knex(tableName).select("*");
+
+    if (isShowing) {
+        query = query
+            .join("movies_theaters", "movies.movie_id", "movies_theaters.movie_id")
+            .where({ "movies_theaters.is_showing": true })
+            .groupBy("movies.movie_id");
+    }
+    return query;
+};
+
+function read(movieId) {
+    return knex(tableName).select("*").where({ movie_id: movieId }).first();
+};
+
+module.exports = {
+    list,
+    read,
+};
