439cffc1ad6a57139b31a609c55b2e04c23a842a
dylanalbios <dylxanj430@gmail.com>
2023-08-09 01:35:09 -0700

    update function added

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -11,14 +11,25 @@ async function reviewExists(req, res, next) {
         return next();
     }
     next({ status: 404, message: `Review cannot be found.` });
-}
+};
 
 
 async function destroy(req, res) {
     await service.delete(res.locals.review.review_id);
     res.sendStatus(204);
-}
+};
+
+async function update(req, res) {
+    const updatedReview = {
+      ...req.body.data,
+      review_id: res.locals.review.review_id,
+    };
+  
+    const data = await service.update(updatedReview);
+    res.json({ data });
+};
 
 module.exports = {
     delete: [asyncErrorBoundary(reviewExists), asyncErrorBoundary(destroy)],
+    update: [asyncErrorBoundary(reviewExists), asyncErrorBoundary(update)],
 };
