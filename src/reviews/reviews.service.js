3013af183628963669afc18ac8170c5afbaef1b3
dylanalbios <dylxanj430@gmail.com>
2023-08-09 01:34:39 -0700

    read function updated to add critics and update function added

--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -3,15 +3,49 @@ const knex = require("../db/connection");
 const tableName = "reviews";
 
 
-const read = (review_id) => {
-    return knex(tableName).select("*").where({ review_id }).first();
-}
-
+const read = async (review_id) => {
+    const review = await knex(tableName)
+      .select("*")
+      .where({ review_id })
+      .first();
+  
+    if (!review) return null;
+  
+    const critic = await knex("critics")
+      .select("*")
+      .where({ critic_id: review.critic_id })
+      .first();
+  
+    return { ...review, critic };
+};
 
 const destroy = (review_id) => {
     return knex(tableName).where({ review_id }).del();
-  }
+};
+
+const update = async (updatedReview) => {
+    const review_id = updatedReview.review_id;
+  
+    const existingReview = await knex(tableName)
+        .select("*")
+        .where({ review_id })
+        .first();
+  
+    if (!existingReview) return null;
+  
+    await knex(tableName)
+        .where({ review_id })
+        .update(updatedReview);
+  
+    const updatedReviewWithCritic = await read(review_id);
+  
+    return updatedReviewWithCritic;
+};
+  
+
+
 module.exports = {
     read,
     delete: destroy,
+    update,
 };
